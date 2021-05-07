8dd2683816a42d204be48e5df6f8d3dfe3540264
tushal012 <tushalkashyap11@gmail.com>
2021-05-07 12:42:24 +0530

    added sub function in calculation.sh

--- calculation.sh
+++ calculation.sh
@@ -8,8 +8,18 @@ function add() {
 	local c=$(( $a + $b ))
 	echo $c
 }
+function sub() {
+        local a=$1
+        local b=$2
+        local c=$(( $a - $b ))
+        echo $c
+}
+
 
 a=20
 b=10
-result="$(( $a $b ))"
+result="$(add $a $b )"
 echo  $result
+
+resultsub="$(sub $a $b )"
+echo  $resultsub
